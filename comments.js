fdb7398b81345dba765efa6cdbe580e81836931c
norbert <norbert.mauger@gmail.com>
2015-01-15 09:35:01 +0100

    data model

--- comments.js
+++ comments.js
@@ -1,12 +1,17 @@
 /**
  * Created by nmauger on 15/01/2015.
  */
+var data = [
+  {author: "Pete Hunt", text: "This is one __comment__"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -15,10 +20,16 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -50,6 +61,6 @@ var Comment = React.createClass({
 });
 
 React.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
